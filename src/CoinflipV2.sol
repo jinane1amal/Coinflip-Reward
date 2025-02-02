f51c03a6b3858eb06b64e19fd7cca6580b33680a
Jinane Amal <144108274+jinane1amal@users.noreply.github.com>
2025-02-02 23:50:37 +0100

    Update CoinflipV2.sol

--- src/CoinflipV2.sol
+++ src/CoinflipV2.sol
@@ -5,12 +5,14 @@ import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Ini
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "./Dauphine.sol"; 
 
 error SeedTooShort();
 
 contract CoinflipV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     string public seed;
+    DauphineToken public token;  // Token state variable
 
     constructor() {
         _disableInitializers();
@@ -20,26 +22,39 @@ contract CoinflipV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         __Ownable_init(initialOwner);
         __UUPSUpgradeable_init();
         seed = "It is a good practice to rotate seeds often in gambling";
+        token = new DauphineToken(); /
     }
 
-    function UserInput(uint8[10] calldata Guesses) external view returns(bool){
+    // Updated UserInput function 
+    function UserInput(uint8[10] calldata Guesses) external returns(bool) {
         uint8[10] memory flips = getFlips();
+        bool win = true;
         for (uint i = 0; i < 10; i++) {
             if (Guesses[i] != flips[i]) {
-                return false; 
+                win = false;
+                break;
             }
         }
-        return true;
-    
-    
+        if (win) {
+            RewardUser(msg.sender);
+        }
+        return win;
     }
-        /// @notice allows the owner of the contract to change the seed to a new one
-    /// @param NewSeed is a string which represents the new seed
+    
+    // New RewardUser function to mint 5 DAU tokens to the winner
+    function RewardUser(address winner) internal {
+        uint256 rewardAmount = 5 * 10 ** 18; // If assuming the token uses 18 decimals
+        token.mint(winner, rewardAmount);
+    }
+
+    /// @notice Allows the owner to change the seed with rotation logic
+    /// @param NewSeed is a string representing the new seed
+    /// @param rotations is the number of rotations to perform on the seed
     function seedRotation(string memory NewSeed, uint rotations) public onlyOwner {
         bytes memory strngInBts = bytes(NewSeed);
         require(strngInBts.length >= 10, "SeedTooShort");
         
-        // rotation logic
+        // Rotation logic: rotates the seed 'rotations' times
         for (uint i = 0; i < rotations; i++) {
             bytes1 lastChar = strngInBts[strngInBts.length - 1];
             for (uint j = strngInBts.length - 1; j > 0; j--) {
@@ -48,34 +63,24 @@ contract CoinflipV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
             strngInBts[0] = lastChar;
         }
         seed = string(strngInBts);
-        
-    
     }
 
-    // -------------------- helper functions -------------------- //
-    /// @notice This function generates 10 random flips by hashing characters of the seed
-    /// @return a fixed 10 element array of type uint8 with only 1 or 0 as its elements
-    function getFlips() public view returns (uint8[10] memory){
-        // TODO: Cast the seed into a bytes array and get its length
+    // -------------------- Helper Functions -------------------- //
+    /// @notice Generates 10 pseudo-random flips using the seed
+    /// @return A fixed array of 10 uint8 values (1 or 0)
+    function getFlips() public view returns (uint8[10] memory) {
         bytes memory stringInBytes = bytes(seed);
         uint seedlength = stringInBytes.length;
-
         uint8[10] memory results;
-
-        // Setting the interval for grabbing characters
         uint interval = seedlength / 10;
-
         for (uint i = 0; i < 10; i++){
-            // Generating a pseudo-random number by hashing together the character and the block timestamp
             uint randomNum = uint(keccak256(abi.encode(stringInBytes[i * interval], block.timestamp)));
-
             results[i] = (randomNum % 2 == 0) ? 1 : 0;
         }
-
-
         return results;
     }
-     function _authorizeUpgrade(address newImplementation)
+    
+    function _authorizeUpgrade(address newImplementation)
         internal
         onlyOwner
         override
