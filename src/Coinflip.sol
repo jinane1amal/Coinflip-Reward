1b3bc7a7aad079d083d7795b90948896b477b721
Jinane Amal <144108274+jinane1amal@users.noreply.github.com>
2025-02-02 23:47:25 +0100

    Update Coinflip.sol

--- src/Coinflip.sol
+++ src/Coinflip.sol
@@ -5,14 +5,14 @@ import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Ini
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import "./Dauphine.sol";
+import "./Dauphine.sol"; 
 
 error SeedTooShort();
 
 contract Coinflip is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     string public seed;
-    DauphineToken public token; 
+    DauphineToken public token;  // State variable for the token
 
     constructor() {
         _disableInitializers();
@@ -22,58 +22,57 @@ contract Coinflip is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         __Ownable_init(initialOwner);
         __UUPSUpgradeable_init();
         seed = "It is a good practice to rotate seeds often in gambling";
-        token = new DauphineToken();
+        token = new DauphineToken(); 
         token.transferOwnership(address(this));
     }
 
-    function UserInput(uint8[10] calldata Guesses) external view returns(bool){
+    // Updated UserInput function 
+    function UserInput(uint8[10] calldata Guesses) external returns(bool) {
         uint8[10] memory flips = getFlips();
+        bool win = true;
         for (uint i = 0; i < 10; i++) {
             if (Guesses[i] != flips[i]) {
-                return false; 
+                win = false;
+                break;
             }
         }
-        return true;
-    
-    
+        if (win) {
+            RewardUser(msg.sender);
+        }
+        return win;
     }
-        /// @notice allows the owner of the contract to change the seed to a new one
-    /// @param NewSeed is a string which represents the new seed
+    
+    // New RewardUser function to mint tokens to the winner
+    function RewardUser(address winner) internal {
+        uint256 rewardAmount = 5 * 10 ** 18; // If assuming token has 18 decimals
+        token.mint(winner, rewardAmount);
+    }
+
+    /// @notice Allows the owner to change the seed
     function seedRotation(string memory NewSeed) public onlyOwner {
-        // TODO: Cast the string into a bytes array so we may perform operations on it
         bytes memory seedBytes = bytes(NewSeed);
-
         uint seedlength = seedBytes.length;
-
-        if (seedlength < 10){
+        if (seedlength < 10) {
             revert SeedTooShort();
         }
-
         seed = NewSeed;
     }
 
-    // -------------------- helper functions -------------------- //
-    /// @notice This function generates 10 random flips by hashing characters of the seed
-    /// @return a fixed 10 element array of type uint8 with only 1 or 0 as its elements
-    function getFlips() public view returns (uint8[10] memory){
+    // -------------------- Helper Functions -------------------- //
+    /// @notice Generates 10 random flips using the seed
+    function getFlips() public view returns (uint8[10] memory) {
         bytes memory stringInBytes = bytes(seed);
         uint seedlength = stringInBytes.length;
-
         uint8[10] memory results;
-
         uint interval = seedlength / 10;
-
         for (uint i = 0; i < 10; i++){
-            // Generating a pseudo-random number by hashing together the character and the block timestamp
             uint randomNum = uint(keccak256(abi.encode(stringInBytes[i * interval], block.timestamp)));
-
             results[i] = (randomNum % 2 == 0) ? 1 : 0;
         }
-
-
         return results;
     }
-     function _authorizeUpgrade(address newImplementation)
+    
+    function _authorizeUpgrade(address newImplementation)
         internal
         onlyOwner
         override
